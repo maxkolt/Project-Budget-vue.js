b58d4462e70b81bdb9348be350493df27068e464
Администратор <12345kolt@gmail.com>
2021-03-16 19:45:58 +0300

    Проект Budget на vue

---
+++ budget-app/src/components/BudgetList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="budget-list-wrap">
+    <ElCard :header="header">
+
+    </ElCard>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BudgetList',
+  props: {
+    list: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data: () => ({
+    header: "Budget List"
+  })
+}
+</script>
+
+<style>
+</style>
